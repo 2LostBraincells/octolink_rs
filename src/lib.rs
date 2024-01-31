3275b4095962f505adf647e23fde936e6cae2e72
me2m8 <elmer.brannstrom@gmail.com>
2024-01-31 23:47:51 +0100

    Added the structs to deserialize the json response descriving the files from the API

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 use std::error::Error;
 
 use reqwest::Client;
-use serde::{Deserialize, Serialize};
 
-mod types;
+pub mod types;
 
 pub struct Printer {
     address: String,
@@ -36,7 +35,7 @@ impl PrinterBuilder {
         self
     }
 
-    /// Build the printer struct 
+    /// Build the printer struct
     pub fn build(self) -> Printer {
         Printer {
             address: self.address,
@@ -50,29 +49,120 @@ impl PrinterBuilder {
 impl Printer {
     pub async fn get_api_version(&self) -> Result<types::ApiVersion, Box<dyn Error>> {
         let url = format!("http://{}:{}/api/version", self.address, self.port);
-        let res = self.client.get(&url).send().await?;
+        let res = self
+            .client
+            .get(&url)
+            .header("X-Api-Key", &self.api_key)
+            .send()
+            .await?;
         let body = res.json::<types::ApiVersion>().await?;
 
         Ok(body)
     }
 
+    /// Returns a struct containing the current connection settings of the printer
+    /// as well as the available printer settings
     pub async fn get_connection(&self) -> Result<types::PrinterConnection, Box<dyn Error>> {
         let url = format!("http://{}:{}/api/connection", self.address, self.port);
-        let res = self.client.get(&url).send().await?;
+        let res = self
+            .client
+            .get(&url)
+            .header("X-Api-Key", &self.api_key)
+            .send()
+            .await?;
         let body = res.json::<types::PrinterConnection>().await?;
 
         Ok(body)
     }
 
     /// Set the connection settings of the printer
-    pub async fn set_connection(&self, connection: types::PrinterConnectionPost) -> Result<(), Box<dyn Error>> {
+    pub async fn set_connection(
+        &self,
+        connection: types::PrinterConnectionCommand,
+    ) -> Result<(), Box<dyn Error>> {
         let url = format!("http://{}:{}/api/connection", self.address, self.port);
-        let res = self.client.post(&url).json(&connection).send().await?;
-        
+        let res = self
+            .client
+            .post(&url)
+            .header("X-Api-Key", &self.api_key)
+            .json(&connection.to_post())
+            .send()
+            .await?;
+
         if res.status().is_client_error() {
             return Err("Incorrect request body".into());
         }
 
         Ok(())
+    }
+
+    /// Will get the printer files and folders in the specified directory
+    pub async fn get_file(
+        &self,
+        location: &str,
+    ) -> Result<types::printer_files::Files, Box<dyn Error>> {
+        let url = format!(
+            "http://{}:{}/api/files/{}",
+            self.address, self.port, location
+        );
+        let res = self
+            .client
+            .get(&url)
+            .header("X-Api-Key", &self.api_key)
+            .send()
+            .await?;
+        let body = res.json::<types::printer_files::Files>().await?;
+
+        Ok(body)
+    }
+
+    /// Will get all printer files and folders recursively from the specified directory
+    pub async fn get_file_recursive(
+        &self,
+        location: &str,
+    ) -> Result<types::printer_files::Files, Box<dyn Error>> {
+        let url = format!(
+            "http://{}:{}/api/files/{}?recursive=true",
+            self.address, self.port, location
+        );
+        let res = self
+            .client
+            .get(&url)
+            .header("X-Api-Key", &self.api_key)
+            .send()
+            .await?;
+        let body = res.json::<types::printer_files::Files>().await?;
+
+        Ok(body)
+    }
+
+    /// Will get the printer files and folders in the root directory
+    pub async fn get_files(&self) -> Result<types::printer_files::Files, Box<dyn Error>> {
+        let url = format!("http://{}:{}/api/files", self.address, self.port);
+        let res = self
+            .client
+            .get(&url)
+            .header("X-Api-Key", &self.api_key)
+            .send()
+            .await?;
+        let body = res.json::<types::printer_files::Files>().await?;
+
+        Ok(body)
+    }
+
+    /// Will get all printer files and folders recursively
+    pub async fn get_files_recursive(
+        &self,
+    ) -> Result<types::printer_files::Files, Box<dyn Error>> {
+        let url = format!("http://{}:{}/api/files?recursive=true", self.address, self.port);
+        let res = self
+            .client
+            .get(&url)
+            .header("X-Api-Key", &self.api_key)
+            .send()
+            .await?;
+        let body = res.json::<types::printer_files::Files>().await?;
+
+        Ok(body)
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -49,25 +49,193 @@ pub struct PrinterProfile {
 // PRINTER CONNECTION SET 
 //
 
-/// This is the struct that is used to model the JSON body of the request
-///
-/// `command`: The command to send to the printer. This can be one of the following:
-///    * "`connect`"
-///    * "`disconnect`"
-///    * "`fake_ack`"
-///
-/// The following fields are only used if the command is "`connect`"
-/// `port`: The port to connect to. Available ports can be gotten by running [`get_connection`]
-/// `baudrate`: The baudrate to connect at. Available baudrates can be gotten by running [`get_connection`]
-/// `printer_profile`: The printer profile to use. Available printer profiles can be gotten by running [`get_connection`]
-/// `save`: Whether or not to save the connection settings as the new preference.
-/// `autoconnect`: Whether or not to automatically connect to the printer on server startup.
+/// This enum is used to model the connection commands that can be sent to the printer.
+pub enum PrinterConnectionCommand {
+    /// `port`: The port to connect to. Available ports can be gotten by running [`get_connection`]
+    /// `baudrate`: The baudrate to connect at. Available baudrates can be gotten by running [`get_connection`]
+    /// `printer_profile`: The printer profile to use. Available printer profiles can be gotten by running [`get_connection`]
+    /// `save`: Whether or not to save the connection settings as the new preference.
+    /// `autoconnect`: Whether or not to automatically connect to the printer on server startup.
+    Connect {
+        port: String,
+        baudrate: u32,
+        printer_profile: String,
+        save: bool,
+        autoconnect: bool,
+    },
+    Disconnect,
+    FakeAck,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PrinterConnectionPost {
     command: String,
-    port: String,
-    baudrate: u32,
-    printer_profile: String,
-    save: bool,
-    autoconnect: bool,
+    port: Option<String>,
+    baudrate: Option<u32>,
+    #[serde(rename = "printerProfile")]
+    printer_profile: Option<String>,
+    save: Option<bool>,
+    autoconnect: Option<bool>,
+}
+
+impl PrinterConnectionCommand {
+    /// Will convert the enum into a struct that can be deserialized to json and sent to the printer.
+    pub fn to_post(self) -> PrinterConnectionPost {
+        match self {
+            PrinterConnectionCommand::Connect {
+                port,
+                baudrate,
+                printer_profile,
+                save,
+                autoconnect,
+            } => PrinterConnectionPost {
+                command: "connect".to_string(),
+                port: Some(port),
+                baudrate: Some(baudrate),
+                printer_profile: Some(printer_profile),
+                save: Some(save),
+                autoconnect: Some(autoconnect),
+            },
+            PrinterConnectionCommand::Disconnect => PrinterConnectionPost {
+                command: "disconnect".to_string(),
+                port: None,
+                baudrate: None,
+                printer_profile: None,
+                save: None,
+                autoconnect: None,
+            },
+            PrinterConnectionCommand::FakeAck => PrinterConnectionPost {
+                command: "fake_ack".to_string(),
+                port: None,
+                baudrate: None,
+                printer_profile: None,
+                save: None,
+                autoconnect: None,
+            },
+        }
+    }
+}
+
+//
+// GET PRINTER FILES
+//
+
+pub mod printer_files {
+    use std::collections::HashMap;
+    use serde::{Deserialize, Serialize};
+
+    /// This is the struct that is returned when getting the files from the printer.
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Files {
+        files: Vec<File>,
+        free: u64,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct File {
+        name: String,
+        display: String, path: String,
+        #[serde(rename = "type")]
+        type_: String,
+        #[serde(rename = "typePath")]
+        type_path: Vec<String>,
+        type_fields: FileType,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(tag = "type")]
+    pub enum FileType {
+        #[serde(rename = "folder")]
+        Folder {
+            children: Vec<File>,
+            size: Option<u64>,
+        },
+        #[serde(rename = "file")]
+        File {
+            hash: Option<String>,
+            size: Option<u64>,
+            date: Option<u64>,
+            origin: String,
+            refs: Option<Refs>,
+            #[serde(rename = "gcodeAnalysis")]
+            gcode_analysis: Option<GcodeAnalysis>,
+            prints: Option<PrintHistory>,
+            statistics: Option<Statistics>,
+        },
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Refs {
+        resource: String,
+        download: Option<String>,
+        model: Option<String>,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct GcodeAnalysis {
+        #[serde(rename = "estimatedPrintTime")]
+        estimated_print_time: Option<f32>,
+        filament: Option<Vec<GCodeAnalysisTools>>,
+        dimensions: Option<GCodeAnalysisDimensions>,
+        printing_area: Option<GCodeAnalysisArea>,
+        travel_area: Option<GCodeAnalysisArea>,
+        travel_dimensions: Option<GCodeAnalysisDimensions>,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct GCodeAnalysisDimensions {
+        #[serde(rename = "width")]
+        x: f32,
+        #[serde(rename = "height")]
+        y: f32,
+        #[serde(rename = "depth")]
+        z: f32,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct GCodeAnalysisArea {
+        #[serde(rename = "minX")]
+        min_x: f32,
+        #[serde(rename = "minY")]
+        min_y: f32,
+        #[serde(rename = "minZ")]
+        min_z: f32,
+        #[serde(rename = "maxX")]
+        max_x: f32,
+        #[serde(rename = "maxY")]
+        max_y: f32,
+        #[serde(rename = "maxZ")]
+        max_z: f32,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct GCodeAnalysisTools {
+        length: f32,
+        volume: f32,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct PrintHistory {
+        success: u32,
+        failure: u32,
+        last: PrintHistoryLast,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct PrintHistoryLast {
+        date: u64,
+        #[serde(rename = "printTime")]
+        print_time: f32,
+        success: bool,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Statistics {
+        #[serde(flatten)]
+        #[serde(rename = "averagePrintTime")]
+        average_print_time: HashMap<String, f32>,
+        #[serde(flatten)]
+        #[serde(rename = "lastPrintTime")]
+        last_print_time: HashMap<String, f32>,
+    }
 }
